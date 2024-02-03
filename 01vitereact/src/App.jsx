3c32fbf71770d97a2dabb9c4e58c8845693dbdd0
Ronn26 <rchachire4@gmail.com>
2024-02-03 23:44:59 +0530

    custom react app

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,8 +2,14 @@ import Chai from "./chai"
 
 
 function App() {
-    return (
-      <Chai/>
+  const username = "chai aur code"
+
+  return (
+    <>
+    <Chai/>
+    <h1>chai aur react {username}</h1>
+    <p>test para</p>
+   </>
   )
 }
 

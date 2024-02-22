b75b6d33239ade296335f1aadc619dc7784873d3
Ronn26 <rchachire4@gmail.com>
2024-02-23 00:49:21 +0530

    commit

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import UserContextProvider from "./context/UserContextProvider"
 
 function App() {
   
   return (
-    <>
+    <UserContextProvider>
       <h1>React with Chai and share is important</h1>
-    </>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
   )
 }
 
